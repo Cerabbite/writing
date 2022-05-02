43ce90b0cb45d59accbd07e31ec114ed8c335e5a
KevinGiesberts <kevin.giesberts@gmail.com>
2022-05-02 09:48:52 +0200

    Update testing_typer.py

--- testing_typer.py
+++ testing_typer.py
@@ -1,11 +1,14 @@
 import typer
 
+__VERSION__ = "1.0.0-Alpha"
+
 app = typer.Typer()
 
 @app.command()
 def hello():
     print("Hello")
 
+@app.command()
 def VERSION():
     print("Your current version is v1.0.0-Alpha")
 
