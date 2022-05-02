a2a45dfb029974f066ff4e35361da15620da2278
KevinGiesberts <kevin.giesberts@gmail.com>
2022-05-02 12:47:50 +0200

    Adding imports

--- tests/testing_reportlab.py
+++ tests/testing_reportlab.py
@@ -1,5 +1,10 @@
-from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.enums import TA_JUSTIFY, TA_CENTER
+from reportlab.lib.pagesizes import letter, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib,styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch, cm
 
 pdf = Canvas("test.pdf")
+pdf.setTitle("test")
 
 pdf.save()
