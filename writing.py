850bb8cb87495c5ad6be9d01391502a6833204ff
KevinGiesberts <kevin.giesberts@gmail.com>
2022-05-02 10:41:21 +0200

    Update writing.py

--- writing.py
+++ writing.py
@@ -9,31 +9,35 @@ app = typer.Typer()
 def WRITING(input_file: str, output_file: str):
     print(input_file, output_file)
 
+def LIST_OF_VERSION(version):
+    release = version
+    release_list = release.split(".")
+    try:
+        thing = release_list[2].split("-")[0]
+        print(1)
+        thing2 = release_list[2].split("-")[1]
+        print(2)
+        release_list[2] = thing
+        print(3)
+        release_list.append(thing2)
+        print(4)
+    except Exception as e:
+        print(e)
+    print(release_list)
+    release = []
+    for i in release_list:
+        release.append(i.split('\n')[0])
+    print(release)
+    return release
+
+
 @app.command()
 def VERSION(check: bool=False):
     print(f"Your current version is v{__VERSION__}")
     if check:
         url = 'https://raw.githubusercontent.com/Cerabbite/writing/main/LATEST_VERSION'
         page = requests.get(url)
-        print(page.text)
-        latest_release = page.text
-        latest_release_list = latest_release.split(".")
-        try:
-            thing = latest_release_list[2].split("-")[0]
-            print(1)
-            thing2 = latest_release_list[2].split("-")[1]
-            print(2)
-            latest_release_list[2] = thing
-            print(3)
-            latest_release_list.append(thing2)
-            print(4)
-        except Exception as e:
-            print(e)
-        print(latest_release_list)
-        latest_release = []
-        for i in latest_release_list:
-            latest_release.append(i.split('\n')[0])
-        print(latest_release)
+        print(LIST_OF_VERSION(page.text))
         #print("No update available")
 
 @app.command()
