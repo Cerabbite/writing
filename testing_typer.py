c4547935ed5aff5fa82bba36ed04dd2444bd8fab
KevinGiesberts <kevin.giesberts@gmail.com>
2022-05-02 09:47:07 +0200

    Adding second function

--- testing_typer.py
+++ testing_typer.py
@@ -2,8 +2,12 @@ import typer
 
 app = typer.Typer()
 
+@app.command()
 def hello():
     print("Hello")
+
+def VERSION():
+    print("Your current version is v1.0.0-Alpha")
 
 if __name__ == "__main__":
     app()
