73fa9ee4b2f00a4d63f04a579949de4580f31f51
KevinGiesberts <kevin.giesberts@gmail.com>
2022-05-02 10:08:36 +0200

    Working on "Checking if update is available"

--- writing.py
+++ writing.py
@@ -1,4 +1,5 @@
 import typer
+import requests
 
 __VERSION__ = "1.0.0-Alpha"
 
@@ -12,7 +13,18 @@ def WRITING(input_file: str, output_file: str):
 def VERSION(check: bool=False):
     print(f"Your current version is v{__VERSION__}")
     if check:
-        print("No update available")
+        url = 'https://raw.githubusercontent.com/Cerabbite/writing/main/LATEST_VERSION'
+        page = requests.get(url)
+        latest_release = page.text
+        latest_release_list = latest_release.split(".")
+        print9
+        print(page.text)
+        #print("No update available")
+
+@app.command()
+def DOWNLOAD():
+    print("You  are currently unable to download the latest version via you command line go to .... to download the latest version")
+    # refer to VERSION() to check if update is available
 
 if __name__ == "__main__":
     app()
