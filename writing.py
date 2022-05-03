0d90d53e421aede455bd4a496824591e7f8fdbc7
KevinGiesberts <kevin.giesberts@gmail.com>
2022-05-03 18:48:53 +0200

    Update writing.py

--- writing.py
+++ writing.py
@@ -13,6 +13,27 @@ import platform
 
 __VERSION__ = "1.0.0-Alpha"
 __LICENSE__ = "MIT License"
+__LICENSETEXT__ = r"""MIT License
+
+Copyright (c) 2022 Kevin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
 
 """
 A single empty line means a new line and double empty line means new paragraph
@@ -233,9 +254,10 @@ def INFO():
     print(platform.system())
     print(__VERSION__)
     print()
-    with open("LICENSE_TEXT.txt") as txt:
-        txt_read = txt.read()
-        print(txt_read)
+    print(__LICENSETEXT__)
+    #with open("LICENSE_TEXT.txt") as txt:
+    #    txt_read = txt.read()
+    #    print(txt_read)
 
 if __name__ == "__main__":
     app()
