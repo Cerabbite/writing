f0c567e59461afcb6b8f24bc82484084c2ded196
KevinGiesberts <kevin.giesberts@gmail.com>
2022-05-04 11:53:06 +0200

    Update writing.py

--- writing.py
+++ writing.py
@@ -1,6 +1,6 @@
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.units import inch, cm
-from reportlab.lib.pagesizes import LETTER, LEGAL, TABLOID, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10
+from reportlab.lib.pagesizes import LETTER, LEGAL, TABLOID, ELEVENSEVENTEEN, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10
 from reportlab.platypus import SimpleDocTemplate, Paragraph, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.pdfbase.ttfonts import TTFont
@@ -217,6 +217,8 @@ def WRITING(input_file: str, output_file: str):
                 return LEGAL
             elif page_size == "TABLOID":
                 return TABLOID
+            elif page_size == "ELEVENSEVENTEEN":
+                return ELEVENSEVENTEEN
 
         story = []
         doc = SimpleDocTemplate(output_file,pagesize=A4,
