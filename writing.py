1f14c0fc19cfe95e3be9fa0b800bdac96a620c40
KevinGiesberts <kevin.giesberts@gmail.com>
2022-05-17 18:58:49 +0200

    FDX Parenthetical Fix

--- writing.py
+++ writing.py
@@ -913,7 +913,7 @@ def screenplay(input_file: str, output_file: str, read: bool=False):
                 elif x[1] == "parenthetical":
                     #story.append(Paragraph(f"({x[0]})", screenplay_parenthetical_style))
                     file.write('        <Paragraph Type="Parenthetical">\n')
-                    file.write(f'            <Text Font="Courier Final Draft">{x[0].upper()}</Text>\n')
+                    file.write(f'            <Text Font="Courier Final Draft">({x[0].upper()})</Text>\n')
                     file.write('        </Paragraph>\n')
                 elif x[1] == "dialogue":
                     #story.append(Paragraph(x[0], screenplay_dialogue_style))
