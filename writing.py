ddf5389d1bd408b8156260870790c41d31ccef41
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-10 08:37:01 +0200

    Update writing.py

--- writing.py
+++ writing.py
@@ -1,4 +1,5 @@
 #Copyright (c) 2022 Cerabbite
+# For speed and exporting the project change to Rust
 from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.units import inch, cm
 from reportlab.lib.pagesizes import LETTER, LEGAL, TABLOID, ELEVENSEVENTEEN, A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, B0, B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, C0, C1, C2, C3, C4, C5, C6, C7, C8, C9, C10
