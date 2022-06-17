0fd8e300d46efa42fa1f9ef3b844055603cc9431
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-17 11:20:12 +0200

    Update novel.rs

--- writing/src/novel.rs
+++ writing/src/novel.rs
@@ -1,3 +1,15 @@
 pub fn novel() {
     
 }
+
+fn DOCX() {
+  println!("You are currently unable to export to DOCX.");
+}
+
+fn PDF() {
+  println!("You are currently unable to export to PDF.");
+}
+
+fn RTF() {
+  println!("You are currently unable to export to RTF.");
+}
