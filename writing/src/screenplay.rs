57fa1315838c6dee8264cd57aa3ab5365d001339
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-17 11:19:02 +0200

    Update screenplay.rs

--- writing/src/screenplay.rs
+++ writing/src/screenplay.rs
@@ -1,3 +1,15 @@
 pub fn screenplay() {
 
 }
+
+fn FDX() {
+  println!("You are currently unable to export to FDX.");
+}
+
+fn PDF() {
+  println!("You are currently unable to export to PDF.");
+}
+
+fn TRELBY() {
+  println!("You are currently unable to export to TRELBY.");
+}
