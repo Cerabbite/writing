64009a5d6bd726544e80b40284629e3e4dfdf92a
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-17 11:21:30 +0200

    Update main.rs

--- writing/src/main.rs
+++ writing/src/main.rs
@@ -5,11 +5,11 @@ use std::env;
 
 fn main() {
     let _args: Vec<String> = env::args().collect();
-    //let target: &String = &_args[1];
+    let target: &String = &_args[1];
 
     setting::version(_args);
 
-    /*if target == "screenplay" {
+    if target == "screenplay" {
         fountain_parser::parser(_args);
-    }*/
+    }
 }
