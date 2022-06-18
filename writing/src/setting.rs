3f684152773708f30f06b18f4fe1605af1a9d736
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-18 16:31:29 +0200

    Update setting.rs

--- writing/src/setting.rs
+++ writing/src/setting.rs
@@ -2,10 +2,20 @@
 
 use crate::ERRORCODES;
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const COPYRIGHT: &str = "Copyright (c) 2022 Cerabbite"
+
 pub fn version(_args: Vec<String>) {
-    println!("{:?}", _args);
+    //println!("{:?}", _args);
+    let release_date: &str = "20-07-2022";
+    println!("writing v{} released on {}", VERSION, release_date);
 }
 
 pub fn update(_args: Vec<String>) {
     error::not_implemented("update");
+}
+
+pub fn license(_args: Vec<String>) {
+    error:not_implemented("license");
+    // Extract license text from Github
 }
