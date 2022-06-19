f14d6ad1ca582c173f25972bec3c97e8109ff761
Cerabbite <98265679+Cerabbite@users.noreply.github.com>
2022-06-19 19:22:34 +0200

    Update novel_parser.rs

--- writing/src/novel_parser.rs
+++ writing/src/novel_parser.rs
@@ -5,3 +5,29 @@ use crate::ERRORCODES;
 pub fn parser(_args: Vec<String>) {
     error::not_implemented("novel-parser");
 }
+
+
+/*
+NOVEL - file format .novel
+
+Start settings with ---
+End settings with ---
+Between --- and --- you can put all of your desired settings
+Possible Settings:
+    - title
+    - author
+    - style
+    - font
+    - font-size
+    - line-spacing
+    - left-margin
+    - right-margin
+    - top-margin
+    - bottom-margin
+
+# Chapter Title
+## Sub chapter Title
+### Sub Sub chapter Title
+Chapter content
+"Dialogue"
+*/
