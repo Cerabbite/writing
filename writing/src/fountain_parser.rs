74d53b43dbef1a0487a97ac892239e1a8e01df22
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-19 17:06:07 +0200

    Update fountain_parser.rs

--- writing/src/fountain_parser.rs
+++ writing/src/fountain_parser.rs
@@ -3,14 +3,16 @@
 use crate::ERRORCODES;
 use std::env;
 use std::fs;
-use std::io::Split;
+//use std::io::Split;
+use std::string::split;
 
 pub fn parser(_args: Vec<String>) {
     //println!("Arguments: {:?}", _args);
-    let contents = fs::read_to_string(r"E:\GitHub\writing\writing\test\test.txt").Split("\n");
+    let contents: Vec<String> = fs::read_to_string(r"E:\GitHub\writing\writing\test\test.txt").split("\n");
 
     //let first_character = contents.chars().nth(0).unwrap();
 
-    println!("{}", contents);
+    println!("{:?}", contents[0]);
+
     error::not_implemented("fountain-parser");
 }
