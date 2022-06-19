e3d892c1b9ca6cdfc0aa3b1ae3d875714add6a3d
Cerabbite <98265679+Cerabbite@users.noreply.github.com>
2022-06-19 19:17:11 +0200

    Update fountain_parser.rs

--- writing/src/fountain_parser.rs
+++ writing/src/fountain_parser.rs
@@ -5,6 +5,8 @@ use std::env;
 use std::fs;
 //use std::io::Split;
 use std::string::split;
+use std::string::String;
+//https://stackoverflow.com/questions/21669722/what-is-the-simplest-way-to-convert-a-string-to-upper-case-in-rust
 
 pub fn parser(_args: Vec<String>) {
     //println!("Arguments: {:?}", _args);
@@ -13,6 +15,19 @@ pub fn parser(_args: Vec<String>) {
     //let first_character = contents.chars().nth(0).unwrap();
 
     println!("{:?}", contents[0]);
+    
+    for i in contents {
+        if i.chars().nth(0).unwrap() == "." and i.chars().nth(0).unwrap() != "." {
+            scene_heading();
+        } else if i[..4].to_uppercase() == "INT." or i[..4] == "EXT." {
+            scene_heading();
+        }
+    }
 
     error::not_implemented("fountain-parser");
+}
+
+
+fn scene_heading() {
+    println!("This is the function for Scene Headings");
 }
