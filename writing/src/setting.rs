aac02d1c351189c27ed7989173d7b10da5d2b327
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-20 08:23:15 +0200

    Start to webscraping

--- writing/src/setting.rs
+++ writing/src/setting.rs
@@ -8,6 +8,13 @@ const COPYRIGHT: &str = "Copyright (c) 2022 Cerabbite";
 pub fn version(_args: Vec<String>) {
     //println!("{:?}", _args);
     let release_date: &str = "20-07-2022";
+    let response = reqwest::blocking::get(
+        "https://raw.githubusercontent.com/Cerabbite/writing/main/Releases",
+    )
+    .unwrap()
+    .text()
+    .unwrap();
+    println!("{:?}", response);
     if release_date == "cannot find" {
         println!("writing v{} this version is not yet public", VERSION);
     } else {
