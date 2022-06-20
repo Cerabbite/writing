80626e58f68a0efea6bef55a70c6f113452f8ae3
Cerabbite <98265679+Cerabbite@users.noreply.github.com>
2022-06-20 22:37:17 +0200

    Update novel.rs

--- writing/src/novel.rs
+++ writing/src/novel.rs
@@ -11,6 +11,15 @@ fn DOCX() {
   //println!("You are currently unable to export to DOCX.");
   // Create a .docx file and rename it to .zip to see the xml content and base everything of of that
   // https://superuser.com/questions/278260/how-do-i-see-the-xml-of-my-docx-document for more information
+  
+  /*
+  To create the .docx file:
+    Create a folder with the output file name as its name in the temp folder of the OS
+    Create the file and folder structure of .docx files
+    Write content to the correct .xml file
+    Once everything is written create a .zip file from the folder and copy that one in to the output folder
+    Rename the .zip file to .docx
+  */
   error::not_implemented("docx-export");
 }
 
