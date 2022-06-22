f623953ec1bd0ea3e6efbf6c7bc5ea10314a2bf0
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-22 08:39:58 +0200

    Update log.rs

--- writing/src/log.rs
+++ writing/src/log.rs
@@ -6,7 +6,18 @@ pub fn CheckFile() {
   // If file does not exist create the file and add writing to environment variable
 }
 
-pub fn Log() {
-  // Write things to the log file in format:
-  // 21-06-2022 08:42: "Here the log information";
+pub fn warn(Warning: String) {
+
+}
+
+pub fn info(Information: String) {
+
+}
+
+pub fn error(Information: String) {
+
+}
+
+pub fn debug(Debug:: String) {
+
 }
