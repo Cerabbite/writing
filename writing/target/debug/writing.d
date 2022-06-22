3de1b22e91cb2460e4aba00dc5822d4dc6d74d1e
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-22 17:02:22 +0200

    Remove Target

--- writing/target/debug/writing.d
+++
@@ -1,1 +0,0 @@
-E:\GitHub\writing\writing\target\debug\writing.exe: E:\GitHub\writing\writing\src\error.rs E:\GitHub\writing\writing\src\fountain_parser.rs E:\GitHub\writing\writing\src\log.rs E:\GitHub\writing\writing\src\main.rs E:\GitHub\writing\writing\src\novel_parser.rs E:\GitHub\writing\writing\src\setting.rs
