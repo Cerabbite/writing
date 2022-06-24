eaaad5b66f0123c2d1af761716df143cf717b99a
Cerabbite <98265679+Cerabbite@users.noreply.github.com>
2022-06-24 17:09:41 +0200

    Update CONFIG_HANDLER.rs

--- writing/src/CONFIG_HANDLER.rs
+++ writing/src/CONFIG_HANDLER.rs
@@ -3,3 +3,9 @@ use directories::ProjectDirs;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
+
+pub fn CONFIG() {
+  // https://www.youtube.com/watch?v=4EmKgrzHfv4
+  // Use .toml as config format
+  // Also save all the log files in that directory
+}
