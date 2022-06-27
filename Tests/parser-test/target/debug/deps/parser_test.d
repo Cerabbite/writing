98a0bc3e305196f6e4193d7072bebe9fb52273f2
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-06-27 08:29:12 +0200

    Working on parser test

--- Tests/parser-test/target/debug/deps/parser_test.d
+++
@@ -1,5 +0,0 @@
-E:\GitHub\writing\Tests\parser-test\target\debug\deps\parser_test.exe: src\main.rs
-
-E:\GitHub\writing\Tests\parser-test\target\debug\deps\parser_test.d: src\main.rs
-
-src\main.rs:
--- Tests/parser-test/target/debug/parser-test.d
+++
@@ -1,1 +0,0 @@
-E:\GitHub\writing\Tests\parser-test\target\debug\parser-test.exe: E:\GitHub\writing\Tests\parser-test\src\main.rs
