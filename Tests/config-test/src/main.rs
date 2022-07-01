6502104eeae06fb4cba51a675753e55ff49adfc6
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-07-01 20:46:15 +0200

    Started working on parsing toml files
    You can read .toml files and print it to the screen
    You can print all the characters of the .toml files to the screen
    You cannot check if a character is equal to ']' or '[' -- This needs
    fixing

--- Tests/config-test/src/main.rs
+++ Tests/config-test/src/main.rs
@@ -1,4 +1,5 @@
-use std::fs::File;
+use std::env;
+use std::fs;
 use std::io::prelude::*;
 
 const CONFIG_CONTENT: &[u8] = b"[writing]
@@ -10,13 +11,34 @@ license: MIT License
 page-size: A4;
 font: some-font.ttf
 font-size: 12
+
 [screenplay]
 page-size: A4";
 
-fn parser() {
+// Convert &str into char or char into &str
+fn main() { //parser() {
+    let contents = fs::read_to_string("config/writing.toml")
+                    .expect("Something went wrong");
 
+    println!("With text:\n{}", contents);
+
+    //let sections;
+    let mut start_section = False;
+    let mut section = String::from("");
+    for i in contents.chars() {
+        if i == "[" {
+            start_section = True;
+        } else if i == "]" && start_section == True {
+            start_section = False;
+            print("Found a section: {}", section);
+            section = String::from("");
+        } else if start_section == True {
+            section.push(i);
+        }
+    }
 }
 
+/*
 // DO NOT DO THIS IN THE MAIN FUNCTION WHEN IMPLEMENTED!!
 fn main() -> std::io::Result<()> {
     // Create path - source: https://stackoverflow.com/questions/59046312/how-can-i-create-a-file-and-its-parent-directories-using-a-single-method-in-rust
@@ -25,10 +47,10 @@ fn main() -> std::io::Result<()> {
     std::fs::create_dir_all(prefix).unwrap();
 
     // Create file - source: https://doc.rust-lang.org/std/fs/struct.File.html
-    let mut file = File::create("config/writing.toml")?;
+    let mut file = fs::File::create("config/writing.toml")?;
     file.write_all(CONFIG_CONTENT)?;
     Ok(())
-}
+}*/
 
 /*use std::fs::File;
 use std::io::prelude::*;
