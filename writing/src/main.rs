a03da6c737c64a8a7ba0acd37d3adc48938be171
Kevin <98265679+Cerabbite@users.noreply.github.com>
2022-07-17 16:45:23 +0200

    Still cant get clap to work properly

--- writing/src/main.rs
+++ writing/src/main.rs
@@ -1,32 +1,15 @@
 mod config;
 
-//use args::writingArgs;
-/*use clap:: {
-    Parser,
-    Arg,
-    App,
-};*/
-use clap::{App, load_yaml};
+use clap::{App, load_yaml, Arg};
 use std::any::Any;
 
-//const data: &dyn Any = config::config();
-//const name: String = config::config().writing.name;
-
 fn main() {
-    //let values = config::config();
-    //println!("{:?}", values.what_is_this());
-    //config::rt("name".to_string());
-    //let data = config::config();
-    //println!("{}", data.writing.name);
-    //println!("{}", name);
-    //let args = writingArgs::parse();
-
     //https://rustrepo.com/repo/clap-rs-clap-rust-command-line#using-yaml
-    //let writing_yaml = load_yaml!("config/writing.yaml");
-    //let matches = App::from(writing_yaml).get_matches();
-
-    let yaml = load_yaml!("..\\config\\writing.yaml");
-    let matches = App::from_yaml(yaml).get_matches();
-
-    println!("{:?}", matches);
+    let writing_yaml = load_yaml!("..\\config\\writing.yaml");
+    let matches = App::from_yaml(writing_yaml).get_matches();
+    //println!("{:?}", matches);
+    let config = matches.value_of("second").unwrap_or("!$@#ERROR*&%");
+    if config != "!$@#ERROR*&%" {
+        println!("{}", config);
+    }
 }
